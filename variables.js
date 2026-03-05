d3bb323490e9320c0eac935e4d23d7c93acfb0ab
Jason Cao <jasoncao54@gmail.com>
2026-03-04 17:43:06 -0800

    Week 1 Day 1 - variables and reassignment practice

---
+++ variables.js
@@ -0,0 +1,48 @@
+// =========================
+// VARIABLES PRACTICE 
+// WEEK 1 DAY 1
+// =========================
+
+let name = "Jason";
+let age = 28;
+let country = "Canada";
+
+console.log(name);
+console.log(age);
+console.log(country);
+
+let userName = "Jason C";
+let favouriteGame = "Pokemon";
+let favouriteSport = "Hockey and Football";
+
+console.log(userName);
+console.log(favouriteGame);
+console.log(favouriteSport); 
+
+let score = 0;
+console.log(score);
+score = 10;
+console.log(score);
+score = 50;
+console.log(score);
+
+let coins = 5;
+console.log(coins);
+coins = coins + 5;
+console.log(coins);
+
+let lives = 3;
+lives = lives - 1;
+lives = lives - 1; 
+console.log(lives);
+
+let points = 10; 
+
+points += 5; 
+points += 5; 
+
+console.log(points);
+
+points -= 8;
+
+console.log(points);
