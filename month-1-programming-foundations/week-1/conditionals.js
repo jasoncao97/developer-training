524de9262ee296825b7556449b1c27c878506aa8
Jason Cao <jasoncao54@gmail.com>
2026-03-23 19:09:07 -0700

    Week 1 Day 4 - conditionals and if statements

---
+++ month-1-programming-foundations/week-1/conditionals.js
@@ -0,0 +1,46 @@
+// ======================================
+// Week 1 Day 4
+// Conditionals (if statements)
+// ======================================
+
+let score = 120;
+
+if (score > 100) {
+    console.log("You Win!");
+}
+
+
+let score2 = 80;
+
+if (score2 > 100) {
+    console.log("You Win!");
+}
+
+
+let score3 = 60;
+
+if (score3 > 100) {
+    console.log("You Win!");
+} else {
+    console.log("Try Again!");
+}
+
+// ======================================
+// Mini Challenge
+// ======================================
+
+let score4 = 95;
+
+if (score4 + 10 > 100) {
+    console.log("Level Up!");
+} else {
+    console.log("Keep Going!");
+}
+
+let score5 = 20;
+
+if (score5 > 100)
+    console.log("Win!");
+    console.log("Will this code run?");
+
+// 2nd line will always run; shows importance of curly brackets
