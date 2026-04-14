e4e31bc342130f1e7f6859dfb6f76a598ac7f415
Jason Cao <jasoncao54@gmail.com>
2026-04-14 15:35:03 -0700

    Week 1 recap - review and challenge

---
+++ month-1-programming-foundations/week-1/week1-recap.js
@@ -0,0 +1,58 @@
+// ======================================
+// Week 1 Recap
+// Mini Test + Challenge
+// ======================================
+
+// Question 1
+console.log("Q1:", "5" + 5 * 2);
+
+// my answer: Q1: 15
+// correct answer: Q1: 510
+
+// Question 2
+let value = 10;
+value += 5;
+console.log("Q2:", value);
+
+// my answer:
+// Q2: 15
+
+// Question 3
+console.log("Q3:", 10 > 5 && 3 > 8);
+
+// my answer:
+// Q3: False 
+
+// Question 4
+console.log("Q4:", 7 % 2 === 0);
+
+// my answer:
+// Q4: False
+
+// Question 5
+let score = 85;
+
+if (score >= 90) {
+  console.log("Q5: A");
+} else if (score >= 80) {
+  console.log("Q5: B");
+} else {
+  console.log("Q5: C");
+}
+
+// my answer:
+// Q5: B
+
+// ex 6 (create own code)
+
+let examScore = 80;
+
+if (examScore >= 90) {
+    console.log("Excellent");
+} else if (examScore >= 70) {
+    console.log("Good");
+} else if (examScore >= 50) {
+    console.log("Pass");
+} else {
+    console.log("Fail");
+}
